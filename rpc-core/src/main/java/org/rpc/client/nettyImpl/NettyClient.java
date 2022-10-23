23e2b53e8405f759cfa60aa79b4b179b35b23317
Jackson_Wu <dengwu.wu@foxmail.com>
2022-10-23 16:40:28 +0800

    初始实现Netty实现的客户端和服务端

---
+++ rpc-core/src/main/java/org/rpc/client/nettyImpl/NettyClient.java
@@ -0,0 +1,76 @@
+package org.rpc.client.nettyImpl;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.AttributeKey;
+import lombok.extern.slf4j.Slf4j;
+import org.rpc.client.RpcClient;
+import org.rpc.codec.CommonDecoder;
+import org.rpc.codec.CommonEncoder;
+import org.rpc.dataTransfer.RpcRequest;
+import org.rpc.dataTransfer.RpcResponse;
+import org.rpc.serializer.impl.JsonSerializer;
+
+
+/**
+ * 基于netty实现的客户端
+ * @author Wu
+ */
+@Slf4j
+public class NettyClient implements RpcClient {
+
+
+    private final String host;
+    private final int port;
+    private static final Bootstrap bootstrap;
+
+    public NettyClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    static {
+        EventLoopGroup group = new NioEventLoopGroup();
+        bootstrap = new Bootstrap();
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast(new CommonDecoder())
+                                .addLast(new CommonEncoder(new JsonSerializer()))
+                                .addLast(new NettyClientHandler());
+                    }
+                });
+    }
+
+    @Override
+    public Object sendRequest(RpcRequest rpcRequest) {
+        try {
+            ChannelFuture future = bootstrap.connect(host, port).sync();
+            log.info("客户端连接到服务器 {}:{}", host, port);
+            Channel channel = future.channel();
+            if(channel != null) {
+                channel.writeAndFlush(rpcRequest).addListener(future1 -> {
+                    if(future1.isSuccess()) {
+                        log.info(String.format("客户端发送消息: %s", rpcRequest.toString()));
+                    } else {
+                        log.error("发送消息时有错误发生: ", future1.cause());
+                    }
+                });
+                channel.closeFuture().sync();
+                AttributeKey<RpcResponse> key = AttributeKey.valueOf("rpcResponse");
+                RpcResponse rpcResponse = channel.attr(key).get();
+                return rpcResponse.getData();
+            }
+        } catch (InterruptedException e) {
+            log.error("发送消息时有错误发生: ", e);
+        }
+        return null;
+    }
+}
---
+++ rpc-core/src/main/java/org/rpc/server/nettyImpl/NettyServer.java
@@ -0,0 +1,56 @@
+package org.rpc.server.nettyImpl;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.rpc.codec.CommonDecoder;
+import org.rpc.codec.CommonEncoder;
+import org.rpc.serializer.impl.JsonSerializer;
+import org.rpc.server.RpcServer;
+
+/**
+ * 基础netty实现的服务端
+ * @author Wu
+ * @date 2022年10月23日 15:26
+ */
+@Slf4j
+public class NettyServer implements RpcServer {
+
+    @Override
+    public void start(int port) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .option(ChannelOption.SO_BACKLOG, 256)
+                    .option(ChannelOption.SO_KEEPALIVE, true)
+                    .childOption(ChannelOption.TCP_NODELAY, true)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new CommonEncoder(new JsonSerializer()));
+                            pipeline.addLast(new CommonDecoder());
+                            pipeline.addLast(new NettyServerHandler());
+                        }
+                    });
+            ChannelFuture future = serverBootstrap.bind(port).sync();
+            future.channel().closeFuture().sync();
+
+        } catch (InterruptedException e) {
+            log.error("启动服务器时有错误发生: ", e);
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
